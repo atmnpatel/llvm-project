1354ed774e2a593cfdd7bdf8aa1c67ce18993469
Atmn Patel <atmndp@gmail.com>
2021-11-19 17:59:17 -0500

    epoll wait

--- openmp/libomptarget/plugins/remote/transports/ucx/Base.cpp
+++ openmp/libomptarget/plugins/remote/transports/ucx/Base.cpp
@@ -3,6 +3,7 @@
 #include "ucs/type/thread_mode.h"
 #include "ucx/Serialization.h"
 #include "llvm/Support/ErrorHandling.h"
+#include <sys/epoll.h>
 
 namespace transport::ucx {
 
@@ -68,6 +69,55 @@ SendFutureTy EndpointTy::asyncSend(const uint64_t Tag, const char *Message,
     ERR("failed to send message {0}\n", ucs_status_string(UCS_PTR_STATUS(Req)))
 
   return {Req, Ctx, Message};
+}
+
+static ucs_status_t poll_wait(ucp_worker_h ucp_worker) {
+  int err            = 0;
+  ucs_status_t ret   = UCS_ERR_NO_MESSAGE;
+  int epoll_fd_local = 0;
+  int epoll_fd       = 0;
+  ucs_status_t status;
+  struct epoll_event ev;
+  ev.data.u64        = 0;
+
+  status = ucp_worker_get_efd(ucp_worker, &epoll_fd);
+  if (UCS_OK != status) {
+    printf("Could not get efd");
+    ERR("Could not get efd")
+    return ret;
+  }
+
+  /* It is recommended to copy original fd */
+  epoll_fd_local = epoll_create(1);
+
+  ev.data.fd = epoll_fd;
+  ev.events = EPOLLIN;
+  err = epoll_ctl(epoll_fd_local, EPOLL_CTL_ADD, epoll_fd, &ev);
+  if (err < 0) {
+    printf("Could not add socket to epoll");
+    close(epoll_fd_local);
+    ERR("Could not add original socket to the new epoll")
+  }
+
+  /* Need to prepare ucp_worker before epoll_wait */
+  status = ucp_worker_arm(ucp_worker);
+  if (status == UCS_ERR_BUSY) { /* some events are arrived already */
+    ret = UCS_OK;
+    close(epoll_fd_local);
+  }
+  if (status != UCS_OK) {
+    printf("ucp_worker_arm");
+    close(epoll_fd_local);
+    ERR("ucp_worker_arm\n");
+  }
+
+  do {
+    err = epoll_wait(epoll_fd_local, &ev, 1, -1);
+  } while ((err == -1) && (errno == EINTR));
+
+  ret = UCS_OK;
+
+  return ret;
 }
 
 std::pair<MessageKind, std::string> WorkerTy::receive(const uint64_t Tag) {
